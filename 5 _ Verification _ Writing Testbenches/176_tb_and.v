f1b4ecb6367794b93a0094fe6a5ced2aaff9307d
Francis <francis.linking@gmail.com>
2021-02-07 12:49:03 +0800

    Verification: Writing Testbenches

---
+++ 5 _ Verification _ Writing Testbenches/176_tb_and.v
@@ -0,0 +1,12 @@
+module top_module();
+    reg [1:0] in;
+    reg out;
+    initial begin
+        in = 2'b00;
+        #10 in = 2'b01;
+        #10 in = 2'b10;
+        #10 in = 2'b11;
+    end
+        
+    andgate andgate1 (in,out);
+endmodule
