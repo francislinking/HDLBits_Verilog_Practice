0285b113f581844460d3805d4e6a7044981faf19
Francis <francis.linking@gmail.com>
2021-02-07 12:38:24 +0800

    Verification: Reading Simulations - Finding bugs

--- 4 _ Verfication _ Reading Simulations/163_bugs_case.v
+++ 4 _ Verfication _ Reading Simulations/163_bugs_case.v
@@ -25,18 +25,20 @@ module top_module (
 	end
 	
     // my solution
-    always @(*)
-        case (code)
-            8'h45: begin out = 0;valid=1; end
-            8'h16: begin out = 1;valid=1; end
-            8'h1e: begin out = 2;valid=1; end
-            8'h26: begin out = 3;valid=1; end
-            8'h25: begin out = 4;valid=1; end
-            8'h2e: begin out = 5;valid=1; end
-            8'h36: begin out = 6;valid=1; end
-            8'h3d: begin out = 7;valid=1; end
-            8'h3e: begin out = 8;valid=1; end
-            8'h46: begin out = 9;valid=1; end
-            default: begin out=0;valid = 0;end
-        endcase
+    // always @(*)
+    //     case (code)
+    //         8'h45: begin out = 0;valid=1; end
+    //         8'h16: begin out = 1;valid=1; end
+    //         8'h1e: begin out = 2;valid=1; end
+    //         8'h26: begin out = 3;valid=1; end
+    //         8'h25: begin out = 4;valid=1; end
+    //         8'h2e: begin out = 5;valid=1; end
+    //         8'h36: begin out = 6;valid=1; end
+    //         8'h3d: begin out = 7;valid=1; end
+    //         8'h3e: begin out = 8;valid=1; end
+    //         8'h46: begin out = 9;valid=1; end
+    //         default: begin out=0;valid = 0;end
+    //     endcase
+
+        
 endmodule
